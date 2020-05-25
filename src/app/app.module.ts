a9ba13e30fee2a4309a3dbacac1275d87d3deacd
Terral hugo <h.terral.jouvet@gmail.com>
2020-05-26 01:20:54 +0200

    Page project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RoutingModule } from './routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core';
 import { UsersModule } from './users';
+import { ProjectModule } from './project';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { UsersModule } from './users';
 
     CoreModule,
 
-    UsersModule
+    UsersModule,
+    ProjectModule
   ],
   providers: [],
   bootstrap: [AppContainer]
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersContainer } from './users';
+import {ProjectContainer} from './project';
+
 
 
 const routes: Routes = [{
   path: '',
   component: UsersContainer
+},
+{
+  path: '/project',
+  component: ProjectContainer
 }];
 
 
