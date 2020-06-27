be32c128e4411fe5ecc9464a63bcf9209658300a
m6hichri <m6hichri@enib.fr>
2020-06-27 19:09:34 +0200

    fixed environent variables issues

--- src/app/adresse-mail/services/mail.service.ts
+++ src/app/adresse-mail/services/mail.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Mail } from '../model/mail.model';
 import { map } from 'rxjs/operators';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 
 @Injectable({
