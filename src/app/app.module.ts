49d81b8f0c696b4bafa25f114193c11634c0cffc
LAURENT Louis <louis_laurent@aol.fr>
2020-07-03 00:00:02 +0200

    add job module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from './core';
 import {UsersModule} from './users';
 import {AdresseMailModule} from './adresse-mail';
+import { JobComponent } from './job/job.component';
 
 
 @NgModule({
   declarations: [
-    AppContainer
+    AppContainer,
+    JobComponent
   ],
   imports: [
     BrowserModule,
