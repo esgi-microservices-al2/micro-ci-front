ef5201173a505eb38b5e9f0f8d22f4a9eb0e8f7a
m6hichri <m6hichri@enib.fr>
2020-07-15 13:46:58 +0200

    fix conflit Url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   apiProject: '`http://localhost:8081/api/v1/projects/',
   production: true,
-  apiUrl: 'http://localhost:8080/notification/',
+  apiUrl: 'http://micro-ci-al2.internal.cloudapp.net:8187/',
   configuration: {
     commandApi: 'http://micro-ci.westus2.cloudapp.azure.com:40503'
   }
